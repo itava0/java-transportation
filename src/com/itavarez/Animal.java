a87227d330e8dcab6582366c206b52d04c44971f
Ivan Tavarez <40846384+itava0@users.noreply.github.com>
2020-02-06 10:58:03 -0500

    Create an interface called animal with multiple methods

---
+++ src/com/itavarez/Animal.java
@@ -0,0 +1,12 @@
+package com.itavarez;
+
+//interface contains methods / no fields
+//Interfaces specify what a class
+// must do and not how. It is the blueprint of the class.
+//inherit from multiple interface
+
+public interface Animal {
+    void move();
+    void eat(int i);
+    String speak();
+}
--- src/com/itavarez/Main.java
+++ src/com/itavarez/Main.java
@@ -1,5 +1,7 @@
 package com.itavarez;
 
+import java.util.*;
+
 public class Main {
 
     public static void main(String[] args) {
